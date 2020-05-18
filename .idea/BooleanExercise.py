345b452f712d39a765cdd8f7df03f4a927ca8166
ooshobe <gigioshobe21@gmail.com>
2020-05-18 13:32:40 -0400

    Initial Commit: Testing out Python with Github

---
+++ .idea/BooleanExercise.py
@@ -0,0 +1,9 @@
+num3 = float (input("Please enter your age: "))
+num4 = float(input("Please enter your friends age: "))
+
+if (num3 > num4):
+    print("You are older.")
+elif(num3 == num4):
+    print("You're both hte same age.")
+else:
+    print("You're friend is older ")
